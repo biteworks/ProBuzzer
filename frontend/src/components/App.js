c73afda0fa436c9aede1422c8804c537012871da
Tobias Wilhelmm <mail@tobiaswilhelm.com>
2021-02-15 16:49:00 +0100

    Renamed fontend to frontend

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>This is my Soundboard!</h1>
+    );
+  }
+}
+
+export default App;
